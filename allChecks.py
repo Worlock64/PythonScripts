216415e6eb47f445b54f1e10707ea78cb6f92b34
Shane Scott-Lewis <shane.scottlewis@gmail.com>
2024-03-06 16:22:32 -0500

    Added a checkReboot function

--- allChecks.py
+++ allChecks.py
@@ -1,4 +1,9 @@
 #!usr/bin/env python3
+import os
+
+def checkRebood():
+    """Returns True if the computer is pending reboot."""
+    return os.path.exist("/run/reboot-required")
 
 def main():
     pass
